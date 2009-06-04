ac2226bfa0a406248f6660bdc118d8b5bc8336d8
Andrew Neil <andrew.jr.neil@gmail.com>
2009-06-04 16:57:38 +0100

    Events index title includes year, if not current.
    So looking at events from August *last year*, the title will be "August,
    2008".

--- app/helpers/page_events_helper.rb
+++ app/helpers/page_events_helper.rb
@@ -1,2 +1,11 @@
 module PageEventsHelper
+  
+  def month_with_year(date)
+    if Time.now.year == date.year
+      date.strftime("%B")
+    else
+      date.strftime("%B, %Y")
+    end
+  end
+  
 end
