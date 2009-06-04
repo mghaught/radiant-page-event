17c91fedd217094f8a9c494b112d9fe5e5688ded
Andrew Neil <andrew.jr.neil@gmail.com>
2009-06-04 17:49:07 +0100

    Start/Finish times each have a table column.
    Finish time has a smart helper, so that it only outputs time, if the
    date is the same as the start time. But if it finishes on a different
    day from its start date, the date is also included.

--- app/helpers/page_events_helper.rb
+++ app/helpers/page_events_helper.rb
@@ -8,4 +8,16 @@ module PageEventsHelper
     end
   end
   
+  def finishing_time(finish, start)
+    if finish.day == start.day and finish.month == start.month and finish.year == start.year
+      finish.strftime("%I:%M %p")
+    else
+      if finish.year == start.year
+        finish.strftime("%d %b, %I:%M %p")
+      else
+        finish.strftime("%d %b %Y, %I:%M %p")
+      end
+    end
+  end
+  
 end
